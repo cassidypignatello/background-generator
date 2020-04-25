e740963993863543db783ce85debcc55da34467b
Cassidy <cassidy.pignatello@gmail.com>
2020-04-25 21:39:38 +0700

    fixes JS error

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ var color2 = document.querySelector('.color2');
 var body = document.querySelector('body');
 
 function setGradient() {
-  var background = body.style.background;
-  background = 'linear-gradient(to right, ' + color1.value + ', ' +  color2.value + ')';
-  css.textContent = background + ';';
+  body.style.background = 'linear-gradient(to right, ' + color1.value + ', ' +  color2.value + ')';
+  css.textContent = body.style.background + ';';
 }
 
 color1.addEventListener('input', setGradient);
