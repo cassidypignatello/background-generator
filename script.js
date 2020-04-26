92a7e94511a8e36616473e75f7373d17f8b63ea5
Cassidy <cassidy.pignatello@gmail.com>
2020-04-26 14:59:59 +0700

    adds button for random colors

--- script.js
+++ script.js
@@ -2,10 +2,25 @@ var css = document.querySelector('h3');
 var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 var body = document.querySelector('body');
+var random = document.querySelector('button');
 
 function setGradient() {
   body.style.background = 'linear-gradient(to right, ' + color1.value + ', ' +  color2.value + ')';
   css.textContent = body.style.background + ';';
+}
+
+function randomizeColors() {
+  body.style.background = 'linear-gradient(to right, ' + getRandomColor() + ', ' +  getRandomColor() + ')';
+  css.textContent = body.style.background + ';';
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 body.onload = function setInputs() {
@@ -14,3 +29,4 @@ body.onload = function setInputs() {
 
 color1.addEventListener('input', setGradient);
 color2.addEventListener('input', setGradient);
+random.addEventListener('click', randomizeColors);
